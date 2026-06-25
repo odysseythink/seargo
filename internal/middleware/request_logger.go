6c1498450977df80d601d15ac6080884f741bba0
ranwei693532 <ranwei693532@163.com>
2026-06-25 13:58:46 +0800

    fix logger format

--- internal/middleware/request_logger.go
+++ internal/middleware/request_logger.go
@@ -21,14 +21,15 @@ func RequestLogger() gin.HandlerFunc {
 		metrics.HTTPRequestDuration.WithLabelValues(c.Request.Method, c.Request.URL.Path).Observe(duration.Seconds())
 
 		rid, _ := c.Get("request_id")
-		mlog.Info("http_request",
-			"request_id", rid,
-			"method", c.Request.Method,
-			"path", c.Request.URL.Path,
-			"status", c.Writer.Status(),
-			"duration_ms", duration.Milliseconds(),
-			"client_ip", c.ClientIP(),
-			"user_agent", c.Request.UserAgent(),
+		mlog.Info("http_request {",
+			"request_id:", rid,
+			",method:", c.Request.Method,
+			",path:", c.Request.URL.Path,
+			",status:", c.Writer.Status(),
+			",duration_ms:", duration.Milliseconds(),
+			",client_ip:", c.ClientIP(),
+			",user_agent:", c.Request.UserAgent(),
+			"}",
 		)
 	}
 }
